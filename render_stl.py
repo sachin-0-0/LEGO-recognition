54c9c108e192fb9af78fe7738352f9920d32db76
sachin-0-0 <141069436+sachin-0-0@users.noreply.github.com>
2023-09-04 19:55:04 -0700

    Update render_stl.py
    added a loop around the code that actually takes the picture, added a line to adjust the camera's focal length, and made it so the camera is always pointing at the object

--- render_stl.py
+++ render_stl.py
@@ -1,22 +1,46 @@
 import bpy
+import math
 
 # Clear existing mesh objects in the scene
 bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_by_type(type='MESH')
 bpy.ops.object.delete()
 
+stl_part_number = 8
 # Import the STL file
-stl_path = "/path/to/your/file.stl" # change this to your file path
+stl_path = f"/Users/sachinchaubal/Desktop/filtered-parts/{stl_part_number}.stl" # change this to your file path
 bpy.ops.import_mesh.stl(filepath=stl_path)
 
-# Optionally, you can set up the camera, lighting, etc. here
+# Assume the imported object is now the active object
+imported_object = bpy.context.active_object
 
 # Set render engine, resolution, and other settings
 bpy.context.scene.render.engine = 'CYCLES'
 bpy.context.scene.render.resolution_x = 1920
 bpy.context.scene.render.resolution_y = 1080
 
-# Render the image
-render_path = "/path/to/save/render.png" # change this to where you want to save the render
-bpy.context.scene.render.filepath = render_path
-bpy.ops.render.render(write_still=True)
+# Select the camera object
+camera = bpy.data.objects['Camera']
+
+# Create and configure a Track To constraint
+track_to = camera.constraints.new(type='TRACK_TO')
+track_to.target = imported_object
+track_to.track_axis = 'TRACK_NEGATIVE_Z'
+track_to.up_axis = 'UP_Y'
+
+# Loop to render 2 images
+for i in range(2):
+    # Move the camera around the object (you can customize these values)
+    angle = math.radians(i * 180)
+    radius = 10 # Distance from object
+    camera.location = (radius * math.sin(angle), radius * math.cos(angle), 10)
+
+    # Set the camera's focal length (in millimeters)
+    camera.data.lens = 50 # you can change this value
+
+    # Set the render path
+    render_path = f"/Users/sachinchaubal/Desktop/images/{stl_part_number}/{stl_part_number}_{i}.png" # change this to where you want to save the render
+    bpy.context.scene.render.filepath = render_path
+
+    # Render the image
+    bpy.ops.render.render(write_still=True)
