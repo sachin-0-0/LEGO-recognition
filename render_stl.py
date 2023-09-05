0008cc6dd6e5b63c58b5b4438a32bf46fbdc03b7
sachin-0-0 <141069436+sachin-0-0@users.noreply.github.com>
2023-09-04 19:37:42 -0700

    Update render_stl.py
    switched to Blender to render the .stls

--- render_stl.py
+++ render_stl.py
@@ -1,42 +1,22 @@
-import numpy as np
-from stl import mesh
-from mpl_toolkits import mplot3d
-from matplotlib import pyplot as plt
-import os
+import bpy
 
-# Specify path for the new folder
-desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
-folder = "images"
-path = os.path.join(desktop, folder)
+# Clear existing mesh objects in the scene
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_by_type(type='MESH')
+bpy.ops.object.delete()
 
-# Create new folder if it doesn't already exist
-if not os.path.exists(path):
-    os.makedirs(path)
+# Import the STL file
+stl_path = "/path/to/your/file.stl" # change this to your file path
+bpy.ops.import_mesh.stl(filepath=stl_path)
 
-# Load the STL files
-your_mesh = mesh.Mesh.from_file('1.stl')
+# Optionally, you can set up the camera, lighting, etc. here
 
-# Create a new plot
-figure = plt.figure()
-axes = mplot3d.Axes3D(figure)
+# Set render engine, resolution, and other settings
+bpy.context.scene.render.engine = 'CYCLES'
+bpy.context.scene.render.resolution_x = 1920
+bpy.context.scene.render.resolution_y = 1080
 
-# Render the STL mesh
-poly = mplot3d.art3d.Poly3DCollection(your_mesh.vectors, linewidths=0.05, alpha=1, edgecolors='r')
-
-# Make the object green
-poly.set_facecolor((0,1,0))
-
-axes.add_collection3d(poly)
-
-# Auto scale to the mesh size
-scale = your_mesh.points.flatten(None)
-axes.auto_scale_xyz(scale, scale, scale)
-
-# Set the background color to white
-axes.set_facecolor((1,1,1))
-
-# Hide the axes
-axes.set_axis_off()
-
-# Save the plot to the new folder
-plt.savefig(os.path.join(path, 'your_image.png'), bbox_inches='tight', pad_inches=0, transparent=True)
+# Render the image
+render_path = "/path/to/save/render.png" # change this to where you want to save the render
+bpy.context.scene.render.filepath = render_path
+bpy.ops.render.render(write_still=True)
