038462516fd061de2730be608df8b88cd11a009b
sachin-0-0 <141069436+sachin-0-0@users.noreply.github.com>
2023-09-04 19:27:28 -0700

    Update render_stl.py
    added code to sort newly created images

--- render_stl.py
+++ render_stl.py
@@ -2,13 +2,23 @@ import numpy as np
 from stl import mesh
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import os
 
-# Load the STL file
+# Specify path for the new folder
+desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
+folder = "images"
+path = os.path.join(desktop, folder)
+
+# Create new folder if it doesn't already exist
+if not os.path.exists(path):
+    os.makedirs(path)
+
+# Load the STL files
 your_mesh = mesh.Mesh.from_file('your_stl_file.stl')
 
 # Create a new plot
 figure = plt.figure()
-axes = mplot3d.Axes3D(figure)
+axes = Axes3D(figure)
 
 # Render the STL mesh
 axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
@@ -17,5 +27,5 @@ axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
 scale = your_mesh.points.flatten()
 axes.auto_scale_xyz(scale, scale, scale)
 
-# Show the plot and save the image
-plt.savefig('your_image.png')
+# Save the plot to the new folder
+plt.savefig(os.path.join(path, 'your_image.png'))
